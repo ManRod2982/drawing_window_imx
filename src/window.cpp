35324e31939da5c7ea0d34be7a132f090e06bb60
Manuel Rodriguez <manuel2982@gmail.com>
2025-12-29 12:55:19 -0500

    Add NPU inference
    Add the ability to run the inference on the NPU and .gitignore

--- src/window.cpp
+++ src/window.cpp
@@ -5,9 +5,11 @@
 //
 // Window definition, this is where the whole layout is defined
 #include "window.h"
+#include "tensorflow/lite/delegates/external/external_delegate.h"
 #include "tensorflow/lite/interpreter_builder.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model_builder.h"
+#include "tensorflow/lite/optional_debug_tools.h"
 
 #include <iostream>
 
@@ -22,7 +24,7 @@ Window::Window() : clear_button("Clear"), predict_button("Predict") {
 
   // Create TFLite interpreter
   // Load model
-  const char *model_path = "cnn.tflite";
+  const char *model_path = "cnn_quant_vela.tflite";
   std::unique_ptr<tflite::FlatBufferModel> model =
       tflite::FlatBufferModel::BuildFromFile(model_path);
   if (!model) {
@@ -39,6 +41,20 @@ Window::Window() : clear_button("Clear"), predict_button("Predict") {
     std::cerr << "Failed to build interpreter" << std::endl;
     throw std::invalid_argument("Invalid interpreter");
   }
+
+  // Setup delegate
+  // Create external delegate option and pass the QNN Delegate library
+  // location so it can be dynamically loaded. Here we assume the delegate is
+  // located at the path /data/local/tmp/qnn_delegate/.
+  TfLiteExternalDelegateOptions external_delegate_options =
+      TfLiteExternalDelegateOptionsDefault("/usr/lib/libethosu_delegate.so");
+  // Create the External Delegate. This will load the QNN Delegate.
+  TfLiteDelegate *external_delegate =
+      TfLiteExternalDelegateCreate(&external_delegate_options);
+
+  // Add External Delegate into TFLite Interpreter to automatically delegate
+  // nodes.
+  interpreter_->ModifyGraphWithDelegate(external_delegate);
 
   // Allocate tensors for the model
   if (interpreter_->AllocateTensors() != kTfLiteOk) {
@@ -85,9 +101,18 @@ void Window::on_clear_clicked() {
 
 // Helper function used to get the index of the
 // maximum value in a Vector
-int get_max_index(std::vector<float> vec) {
+int get_max_index(std::vector<int8_t> vec) {
   auto max_iter = std::max_element(vec.begin(), vec.end());
   return std::distance(vec.begin(), max_iter);
+}
+
+std::vector<int8_t> quantize(std::vector<float> vec) {
+  std::vector<int8_t> res;
+  for (const auto &num : vec) {
+    res.push_back((num / 0.00392157) + 128);
+  }
+
+  return res;
 }
 
 // Calls the save_screen method on the MouseDrawing area
@@ -95,22 +120,51 @@ void Window::on_predict_clicked() {
   // Save screen to file
   std::cout << "Predict clicked!" << std::endl;
   auto drawing = mouse_drawing.export_to_vector(28, 28, 255.0);
+
+  // Print debug info
+  tflite::PrintInterpreterState(interpreter_.get());
+  std::cout << "Tensors size: " << interpreter_->tensors_size() << std::endl;
+  std::cout << "Nodes size: " << interpreter_->nodes_size() << std::endl;
+  std::cout << "Inputs: " << interpreter_->inputs().size() << std::endl;
+  std::cout << "Input 0 name: " << interpreter_->GetInputName(0) << std::endl;
+  int t_size = interpreter_->tensors_size();
+  for (int i = 0; i < t_size; i++) {
+    if (interpreter_->tensor(i)->name)
+      std::cout << i << ": " << interpreter_->tensor(i)->name << ", "
+                << interpreter_->tensor(i)->bytes << ", "
+                << interpreter_->tensor(i)->type << ", "
+                << interpreter_->tensor(i)->params.scale << ", "
+                << interpreter_->tensor(i)->params.zero_point << std::endl;
+  }
+
+  int input_test = interpreter_->inputs()[0];
+  std::cout << "input: " << input_test << std::endl;
+  const std::vector<int> inputs = interpreter_->inputs();
+  const std::vector<int> outputs = interpreter_->outputs();
+  std::cout << "number of inputs: " << inputs.size();
+  std::cout << "number of outputs: " << outputs.size();
+
   // Fill input buffer
-  float *input = interpreter_->typed_input_tensor<float>(0);
-  std::memcpy(input, drawing.data(), drawing.size() * sizeof(float));
+  std::cout << "Filling input buffer" << std::endl;
+  auto quant = quantize(drawing);
+  int8_t *input = interpreter_->typed_input_tensor<int8_t>(0);
+  std::cout << quant.size() * sizeof(int8_t) << std::endl;
+  std::memcpy(input, quant.data(), quant.size() * sizeof(int8_t));
 
   // Invoke TFLite model
+  std::cout << "Invoking model" << std::endl;
   if (interpreter_->Invoke() != kTfLiteOk) {
     std::cerr << "Failed to invoke Interpreter!" << std::endl;
   }
 
   // Get results
-  float *output = interpreter_->typed_output_tensor<float>(0);
-  std::vector<float> output_vec(10);
-  std::memcpy(output_vec.data(), output, sizeof(float) * output_vec.size());
+  std::cout << "Get results!" << std::endl;
+  int8_t *output = interpreter_->typed_output_tensor<int8_t>(0);
+  std::vector<int8_t> output_vec(10);
+  std::memcpy(output_vec.data(), output, sizeof(int8_t) * output_vec.size());
 
   for (size_t i = 0; i < output_vec.size(); i++) {
-    std::cout << i << ": " << output_vec[i] << std::endl;
+    std::cout << i << ": " << std::to_string(output_vec[i]) << std::endl;
   }
 
   int number = get_max_index(output_vec);
